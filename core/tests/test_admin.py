187063174ac89f4c6a4735d4f3346a1e213d9566
igor santos <igr.exe@gmail.com>
2014-11-07 13:00:55 -0200

    reorganized test files

---
+++ core/tests/test_admin.py
@@ -0,0 +1,13 @@
+# from django.test import TestCase
+# from django.contrib.admin.sites import AdminSite
+# from core.admin import EntryAdmin
+# from core.models import Entry
+
+
+# class EntryAdminTest(TestCase):
+
+#     def test_save_entry(self):
+#         """
+#         should save request user like author entry
+#         """
+#         entry_admin = EntryAdmin(Entry, EntryAdmin)
